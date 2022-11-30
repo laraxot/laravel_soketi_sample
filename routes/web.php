c48bc7eb7fe34b4201547eb317c42fc885d5c0d6
marco76tv <marco.sottana@gmail.com>
2022-11-30 13:01:12 +0100

    .

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Events\PublicEvent;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +23,12 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/color', function () {
+    return view('color-picker');
+});
+
+Route::post('/fireEvent', function (Request $request) {
+
+    PublicEvent::dispatch($request->color);
+})->name('fire.public.event');
